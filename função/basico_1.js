7245ce09770e5b9d0393447551a3622583aac356
Matheus-hora48 <matheus11hora@gmail.com>
2022-06-03 11:26:43 -0300

    Novos

---
+++ função/basico_1.js
@@ -0,0 +1,20 @@
+// function declaration
+function bomdia(){
+  console.log('Bom dia!')
+}
+
+bomdia()
+
+// function expression
+const boatarde = function(){
+  console.log('Boa tarde!')
+}
+
+boatarde()
+
+function somar(a,b){
+  return a + b
+}
+
+const resultado = somar(5,4)
+console.log(resultado)
